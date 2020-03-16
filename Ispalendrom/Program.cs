cfd95a3cd8c5a408aa293c9d3d70ee5b5f30334b
Tara Pun <pun.tara@outlook.com>
2020-03-15 21:06:36 -0700

    first commit

---
+++ Ispalendrom/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Ispalendrom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string x= "Rabr";
+            bool y=ispalindrome(x);
+            Console.WriteLine(x+" "+y);
+        }
+        public static bool ispalindrome(string s)
+        {
+            string text = s.ToLower();
+            if (text.Length == 1)
+            {
+                return false;
+            }
+            int start = 0;
+            int end = text.Length-1;
+            while (true)
+            {
+                if (text[start] != text[end])
+                {
+                    return false;
+                }
+                if (start>end)
+                {
+                    return true;
+                }
+                
+                start++;
+                end--;
+            }
+            
+        }
+    }
+}
